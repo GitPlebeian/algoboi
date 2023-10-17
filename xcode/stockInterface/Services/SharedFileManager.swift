03cb9ae0a1bb41820bf2bb6a2a829f08a39fce82
Jackson Tubbs <slimboi@Jacksons-Air.socal.rr.com>
2023-10-17 00:17:20 -0700

    forces the file to save to /desktop/algoboi/shared. its not the best solution but Im done trying to figure it out for now lol.

--- xcode/stockInterface/Services/SharedFileManager.swift
+++ xcode/stockInterface/Services/SharedFileManager.swift
@@ -13,28 +13,14 @@ class SharedFileManager {
     
     func writeDataToFileName(data: Data, fileName: String) {
         
-//        guard let resourcePath = Bundle.main.resourcePath else {
-//            print("Could not get resource path.")
-//            return
-//        }
-//        
-//        // 2. Construct a URL for the "algoboi" folder.
-//        // Assuming the structure is: algoboi > Xcode > YourApp.app
-//        let algoboiFolderURL = URL(fileURLWithPath: resourcePath).deletingLastPathComponent().deletingLastPathComponent()
-//        
-//        let fileURL = algoboiFolderURL.appendingPathComponent(fileName)
+        var url = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!
+        url = url.appendingPathComponent("/algoboi/shared/\(fileName).json")
         
-        let desktopURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        print(desktopURL)
-//
-//        let projectDirectory = FileManager.default.currentDirectoryPath
-//        let directoryAbove = URL(fileURLWithPath: projectDirectory).deletingLastPathComponent()
-//        let fileURL = directoryAbove.appendingPathComponent("\(fileName).json")
-//        do {
-//            try data.write(to: fileURL)
-//        } catch let e {
-//            fatalError("Error writing file to file name: \(e)")
-//        }
+        do {
+            try data.write(to: url)
+        } catch let e {
+            fatalError("Error writing file to file name: \(e)")
+        }
     }
     
     func writeStockAggregateToTestFile(_ model: StockAggregate) {
