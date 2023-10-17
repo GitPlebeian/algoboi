5cee98955d22c06fde0fa2378e1e5cbb2f60abe1
Jackson Tubbs <slimboi@Jacksons-Air.socal.rr.com>
2023-10-16 23:41:53 -0700

    save before deleting the sandbox for a second time lol

--- xcode/stockInterface/Services/SharedFileManager.swift
+++ xcode/stockInterface/Services/SharedFileManager.swift
@@ -12,14 +12,29 @@ class SharedFileManager {
     static let shared = SharedFileManager()
     
     func writeDataToFileName(data: Data, fileName: String) {
-        let projectDirectory = FileManager.default.currentDirectoryPath
-        let directoryAbove = URL(fileURLWithPath: projectDirectory).deletingLastPathComponent()
-        let fileURL = directoryAbove.appendingPathComponent("\(fileName).json")
-        do {
-            try data.write(to: fileURL)
-        } catch let e {
-            fatalError("Error writing file to file name: \(e)")
-        }
+        
+//        guard let resourcePath = Bundle.main.resourcePath else {
+//            print("Could not get resource path.")
+//            return
+//        }
+//        
+//        // 2. Construct a URL for the "algoboi" folder.
+//        // Assuming the structure is: algoboi > Xcode > YourApp.app
+//        let algoboiFolderURL = URL(fileURLWithPath: resourcePath).deletingLastPathComponent().deletingLastPathComponent()
+//        
+//        let fileURL = algoboiFolderURL.appendingPathComponent(fileName)
+        
+        let desktopURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        print(desktopURL)
+//
+//        let projectDirectory = FileManager.default.currentDirectoryPath
+//        let directoryAbove = URL(fileURLWithPath: projectDirectory).deletingLastPathComponent()
+//        let fileURL = directoryAbove.appendingPathComponent("\(fileName).json")
+//        do {
+//            try data.write(to: fileURL)
+//        } catch let e {
+//            fatalError("Error writing file to file name: \(e)")
+//        }
     }
     
     func writeStockAggregateToTestFile(_ model: StockAggregate) {
