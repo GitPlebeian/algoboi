91f105bf66cfa89fda53e211008c5262e6296697
CHONK <chonk@CHONKsMacStudio.socal.rr.com>
2023-10-24 02:50:51 -0700

    python code might actually work. It got 248% on apple on a 4 year timespan where buy and hold only got 182%

--- python/bot2/test.py
+++ python/bot2/test.py
@@ -3,17 +3,19 @@ import torch.nn as nn
 import torch.optim as optim
 import random
 from collections import deque
+import json
+import os
 
 # Define the DNN
 class DNN(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(DNN, self).__init__()
         self.fc = nn.Sequential(
-            nn.Linear(input_dim, 32),
+            nn.Linear(input_dim, 128),
             nn.ReLU(),
-            nn.Linear(32, 32),
+            nn.Linear(128, 128),
             nn.ReLU(),
-            nn.Linear(32, output_dim)
+            nn.Linear(128, output_dim)
         )
     
     def forward(self, x):
@@ -32,49 +34,90 @@ class ReplayBuffer:
         state, action, reward = zip(*random.sample(self.buffer, batch_size))
         return state, action, reward
 
+    def reset(self, capacity):
+        self.buffer = deque(maxlen = capacity)
+
     def __len__(self):
         return len(self.buffer)
 
 # Parameters
-INPUT_DIM = 2  # Excluding the closing price
-OUTPUT_DIM = 3  # buy, sell, hold
-BATCH_SIZE = 32
-LEARNING_RATE = 0.001
+STATE_SIZE = 2  # Excluding the closing price
+ACTION_SIZE = 3  # buy, sell, hold
+BATCH_SIZE = 700
+LEARNING_RATE = 0.0005
 CAPACITY = 10000  # Experience replay buffer capacity
 
 # Initialize network, optimizer, and buffer
-network = DNN(INPUT_DIM, OUTPUT_DIM)
+network = DNN(STATE_SIZE, ACTION_SIZE)
 optimizer = optim.Adam(network.parameters(), lr=LEARNING_RATE)
 buffer = ReplayBuffer(CAPACITY)
 loss_fn = nn.MSELoss()
 
+# Environment Parameters
+epsilon = 1.0  # exploration rate
+epsilon_min = 0.01
+epsilon_decay = 0.995
+starting_portfolio = 100
+
+random_action_count = 0
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the test.json file
+json_path = os.path.join(current_dir, '..', '..', 'shared', 'test.json')
+
+# Read and load the JSON data
+with open(json_path, 'r') as f:
+    states = json.load(f)
+
 # Assuming you have a method to select action based on epsilon-greedy or other policy
 def select_action(state):
-    with torch.no_grad():
-        return network(state).argmax().item()
+    action_value = 0
+    if random.random() <= epsilon:
+        global random_action_count
+        action_value = random.randint(0, ACTION_SIZE - 1)
+        random_action_count = random_action_count + 1
+    else:
+        with torch.no_grad():
+            action_value = network(torch.tensor(state)).argmax().item()
+    return action_value
 
-# Assuming you have a method to compute reward at the end of the episode
-def compute_episode_reward(closing_prices):
-    return (closing_prices[-1] - closing_prices[0]) / closing_prices[0]
+num_episodes = 1500
 
-# Training Loop (for one episode)
-for state in states:
-    # Extracting only the first two values for decision making
-    action_input = torch.tensor(state[:2], dtype=torch.float32)
-    action = select_action(action_input)
 
-    # You can then use 'action' to interact with your trading environment, get new state, etc.
-    # ...
+for episode in range(num_episodes):
+    # print(f"Epsilon {epsilon}")
+    current_buy_price = None
+    portfolio_value = starting_portfolio
+    total_reward = None
+    actions = []
+    random_action_count = 0
+    buffer.reset(CAPACITY)
+    for index, state in enumerate(states):
+        action = select_action(state[1:])
+        actions.append(action)
+        if action == 0 and current_buy_price == None: # Buy Stock
+            current_buy_price = state[0]
 
-    # After completing the episode, compute reward
-    closing_prices = [x[2] for x in states]
-    episode_reward = compute_episode_reward(closing_prices)
-    
-    # Add all state-action pairs with the reward to the replay buffer
-    for s in states:
-        buffer.push(s[:2], action, episode_reward)
+            # print(f"{index} Buying at price: {current_buy_price}")
+        elif action == 1 and current_buy_price != None: # Sell Stock
+            closing_price = state[0]
+            percentage_change = (closing_price - current_buy_price) / current_buy_price
+            portfolio_value = portfolio_value * (percentage_change + 1)
+            current_buy_price = None
+            # print(f"{index} Selling {percentage_change * 100}% | ${portfolio_value}")
 
-    # Sample from buffer and update network
+    total_reward = (portfolio_value - starting_portfolio) / starting_portfolio
+    print(f"Ending Value {total_reward} Random Count {random_action_count}")
+
+    if epsilon * epsilon_decay >= epsilon_min:
+        epsilon = epsilon * epsilon_decay
+    else:
+        epsilon = 0
+
+    for index, state in enumerate(states):
+        buffer.push(state[1:], actions[index], total_reward)
+
     if len(buffer) >= BATCH_SIZE:
         sampled_states, sampled_actions, sampled_rewards = buffer.sample(BATCH_SIZE)
         sampled_states = torch.tensor(sampled_states, dtype=torch.float32)
@@ -83,7 +126,7 @@ for state in states:
 
         # Forward pass
         predicted_rewards = network(sampled_states).gather(1, sampled_actions.unsqueeze(-1)).squeeze(-1)
-        
+
         # Compute loss and backpropagate
         loss = loss_fn(predicted_rewards, sampled_rewards)
         optimizer.zero_grad()
